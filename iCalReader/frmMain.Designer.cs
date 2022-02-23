36cc0e6f3fd237f345c7257730cf8dbf2fc8a1f7
Göran Roseen <goran@roseen.se>
2022-02-23 15:15:19 +0100

    Add project files.

---
+++ iCalReader/frmMain.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace iCalReader
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInfo.Location = new System.Drawing.Point(20, 18);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.Size = new System.Drawing.Size(765, 411);
+            this.txtInfo.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtInfo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "iCal Reader";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInfo;
+    }
+}
+
---
+++ iCalReader/frmMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Ical.Net;
+
+namespace iCalReader
+{
+    public partial class frmMain : Form
+    {
+        string _filename;
+        public frmMain(string filename = null)
+        {
+            _filename = filename;
+
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            if (_filename == null)
+                return;
+
+            var calendar = Calendar.Load(new StreamReader(_filename, true));
+            var events = calendar.Events;
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var ev in events)
+            {
+                sb.Append("Summary:     " + ev.Summary + Environment.NewLine);
+                sb.Append("Location:    " + ev.Location + Environment.NewLine);
+                sb.Append("Start:       " + ev.Start + Environment.NewLine);
+                sb.Append("Duration:    " + ev.Duration + Environment.NewLine);
+                sb.Append("End:         " + ev.End + Environment.NewLine);
+                sb.Append("------------ " + Environment.NewLine);
+                sb.Append("Attendees:   " + Environment.NewLine);
+                foreach (var att in ev.Attendees)
+                {
+                    sb.Append("* " + att.CommonName + Environment.NewLine);
+                }
+                sb.Append("------------ " + Environment.NewLine);
+                sb.Append("Description: " + ev.Description + Environment.NewLine);
+            }
+
+            txtInfo.Text = sb.ToString();
+
+            txtInfo.Select(0,0);
+        }
+    }
+}
